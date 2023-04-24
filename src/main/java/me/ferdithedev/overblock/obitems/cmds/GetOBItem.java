11ab42c670e0927661d4dae78c9fea3a16e613a6
ferdithedev <linastomsuden@outlook.de>
2023-04-24 17:34:14 +0200

    added amount argument

--- src/main/java/me/ferdithedev/overblock/obitems/cmds/GetOBItem.java
+++ src/main/java/me/ferdithedev/overblock/obitems/cmds/GetOBItem.java
@@ -3,11 +3,13 @@ package me.ferdithedev.overblock.obitems.cmds;
 import me.ferdithedev.overblock.obitems.ItemPackage;
 import me.ferdithedev.overblock.obitems.OBItem;
 import me.ferdithedev.overblock.OverBlock;
+import me.ferdithedev.overblock.util.BetterTeleport;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,11 @@ public class GetOBItem implements CommandExecutor, TabCompleter {
             if(args.length > 0) {
                 for(OBItem obitem : OverBlock.getItemManager().getOBItems()) {
                     if(args[0].equalsIgnoreCase(obitem.getInternalName()) || args[0].equalsIgnoreCase(obitem.getItemPackage().getInternalName().toLowerCase() + ":" + obitem.getInternalName())) {
-                        ((Player) sender).getInventory().addItem(obitem.getItemStack());
+                        int amount = 1;
+                        if(args.length > 1 && BetterTeleport.isNumeric(args[1])) amount = Integer.parseInt(args[1]);
+                        ItemStack add = obitem.getItemStack();
+                        add.setAmount(amount);
+                        ((Player) sender).getInventory().addItem(add);
                         if(!obitem.isEnabled()) sender.sendMessage("§c§lWARINING! §cThis item is currently not available in-game since it's not enabled in the 'items.yml' at §e'" + obitem.getPlugin().getName() + "->" + obitem.getInternalName() + "->enabled'§c!");
                     }
                 }
